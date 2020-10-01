57b723e1b54644d2c0637da4ce8cc01d16255f23
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-01 23:44:42 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,11 +4,13 @@ def mario
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
 
 def link 
+  link_phrase = "It's Dangerous To Go Alone! Take This."
+  puts link_phrase
 end
 
 def all_phrases
